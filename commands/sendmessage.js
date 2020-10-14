aba17d31f89826513c83a35bc1a542a178978044
theUpsider <theupsider@gmx.de>
2020-10-14 19:07:29 +0200

    fix: channel find

--- commands/sendmessage.js
+++ commands/sendmessage.js
@@ -6,14 +6,15 @@ module.exports =
     name: 'sendmessage',
     description: 'send a message as the bot',
     args: true,
-    usage: '<channelID> <message>',
+    usage: '<channel name> <message>',
     guildOnly: false,
     aliases:['sendm'],
     async execute(message, args) {
 
         if (!message.member.roles.cache.has(settings.roles.staffrole)) return message.reply(`:x: You do not have permission to execute this command.`)
 
-        (await message.guild.channels.cache.find(args[0]).fetch()).send(args[1])
+        const channel = client.channels.cache.find(args[0])
+        channel.send(args[1])
         //message.guild.channel.get(args[0]).send(args[1])
 
         return;
