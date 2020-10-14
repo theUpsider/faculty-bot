75388ce81b23349018627b128a031c580fb106e6
theUpsider <theupsider@gmx.de>
2020-10-14 19:25:28 +0200

    feat: rupdate

--- commands/rulesupdate.js
+++ commands/rulesupdate.js
@@ -6,14 +6,17 @@ module.exports =
     name: 'rupdate',
     description: 'updates the ruleset',
     args: true,
-    usage: '<new ruleset>',
+    usage: '<"new ruleset">',
     guildOnly: false,
     async execute(message, args) {
 
         if (!message.member.roles.cache.has(settings.roles.staffrole)) return message.reply(`:x: You do not have permission to execute this command.`)
 
         if(settings.channels.rules && settings.messages.rules)
-            (await message.guild.channels.cache.find(channel => channel.id == settings.channels.rules).fetch()).fetchMessage(settings.messages.rules).edit(args[0])
+            (await message.guild.channels.cache.find(channel => channel.id == settings.channels.rules).fetch())
+            .messages.fetch(settings.messages.rules)
+            .then(message => message.edit(args[0]).replace('"','')) // remove symbols
+            .catch(console.error);
         else
             message.reply('wrong arguments supplied')
 
