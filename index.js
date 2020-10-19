964ee24ee50f5045ca62ba37a09ce07ec32bc46a
theUpsider <theupsider@gmx.de>
2020-10-19 13:13:36 +0200

    chores: console

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ imap.on('mail', function (msg) {
 							console.log(`Mail subject: `,Imap.parseHeader(buffer).subject)
 							console.log(`Got displayName: `,displayName)
 							const guild = await bot.guilds.cache.find(server => server.id == settings.guildid).fetch();
+							console.log('Guild: ',guild)
 							const memberToAdd = await guild.members.cache.find(member => member.displayName == displayName).fetch();
 							console.log(`Member to add: `,memberToAdd.displayName)
 							// if mail not registered, do verification
