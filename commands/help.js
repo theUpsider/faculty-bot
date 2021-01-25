649a61ce1438bd5e5c71ae87854e63728118ca1d
theUpsider <theupsider@gmx.de>
2021-01-25 12:50:21 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,12 +12,7 @@ module.exports = {
 
     if (!args.length) {
       data.push("Here's a list of all my commands:");
-      data.push(
-        commands
-          .map((command) => command.name)
-          .filter((cmd) => cmd.admin === false)
-          .join(", ")
-      );
+      data.push(commands.map((command) => command.name).join(", "));
       data.push(
         `\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`
       );
