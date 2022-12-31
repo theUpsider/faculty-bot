5f8c3d5f2c1a2068e26c2b72b6b12c3b0f29070a
rndrmu <jhallwachs36@gmail.com>
2022-12-31 21:11:13 +0100

    add systemd/openrc service file

--- launch_bot.sh
+++ launch_bot.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
-# This script is used to launch the bot via docker
+# This script is used to launch the bot
+
+# Check launch mode (openrc, systemd or docker)
+case $1 in
+    openrc)
+        echo "Launching bot via openrc..."
+        sudo cp faculty-manager.service /etc/init.d/faculty-manager
+        sudo rc-update add faculty-manager
+        sudo rc-service faculty-manager start
+        ;;
+    systemd)
+        echo "Launching bot via systemd..."
+        sudo cp faculty-manager.service /etc/systemd/system/faculty-manager.service
+        sudo systemctl enable --now faculty-manager
+        ;;
+    docker)
+        echo "Launching bot via docker..."
+        launch_docker.sh
+        ;;
+    *)
+        echo "Invalid launch mode!"
+        echo "Usage: launch_bot.sh [openrc|systemd|docker]"
+        exit 1
+        ;;
+esac
 
 # Build the docker image
+
+function launch_docker() {
+    echo "Building docker image..."
+    docker build -t faculty-manager . > /dev/null
+
+    echo "Launching Bot..."
+    docker run -d --name="faculty_manager" --net host --env-file .env faculty_manager:latest
+}
 echo "Building docker image..."
 docker build -t faculty-manager . > /dev/null
 
